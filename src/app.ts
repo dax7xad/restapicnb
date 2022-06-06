0019776ff4c0031a746193a09f19a7a995c4fd4a
Alvaro Dax Diaz Amaya <dax7xad@gmail.com>
2022-06-06 13:07:24 -0600

    funcionando

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,28 @@ import Server from './server/server';
 const soapRequest = require('easy-soap-request');
 const fs = require('fs');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" //this is insecure 
 const xml = fs.readFileSync('zip-code-envelope.xml', 'utf-8');
 // example data
-const url = 'https://graphical.weather.gov/xml/SOAP_server/ndfdXMLserver.php';
+const url = 'https://servicios.bcn.gob.ni/Tc_Servicio/ServicioTC.asmx?WSDL';
 const sampleHeaders = {
   'user-agent': 'sampleTest',
   'Content-Type': 'text/xml;charset=UTF-8',
-  'soapAction': 'https://graphical.weather.gov/xml/DWMLgen/wsdl/ndfdXML.wsdl#LatLonListZipCode',
+  'soapAction': 'http://servicios.bcn.gob.ni/RecuperaTC_Dia',
 };
 
 // Import de las rutas
 import routerApp from './routes/app';
 
 const server = Server.init();
-
+// usage of module
+const fnTipoCambio = async () => {
+  const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 1000 }); // Optional timeout parameter(milliseconds)
+  const { headers, body, statusCode } = response;
+  console.log(headers);
+  console.log(body);
+  console.log(statusCode);
+};
 // Rutas
 server.app.use( '/', routerApp );
 
@@ -25,16 +33,9 @@ server.app.use( '/', routerApp );
 server.start( () => {
     console.log('Express Server state:'+ colors.green(' ====> OK') )
     console.log('Express Server port: '+ colors.green(` ====> OK ${server.port.toString()}`));
-  (async () => {
-    const { response } = await soapRequest({ url: url, headers: sampleHeaders, xml: xml, timeout: 1000 }); // Optional timeout parameter(milliseconds)
-    const { headers, body, statusCode } = response;
-    console.log(headers);
-    console.log(body);
-    console.log(statusCode);
-  })();
+
   });
 
-// usage of module
 
 
 
